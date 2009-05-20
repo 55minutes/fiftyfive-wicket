22776197d4391014ea7c10ab8c5cbbd888cf00aa
Richa Avasthi <ravasthi@55minutes.com>
2009-05-20 17:58:40 +0000

    Add tests for !CalendarControl component. Closes #7.
    git-svn-id: http://opensource.55minutes.com/svn/java/trunk/wicket@22 3a26569e-5468-0410-bb2f-b495330926e5

---
+++ wicket/src/test/java/fiftyfive/wicket/datetime/CalendarControlTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2009 55 Minutes (http://www.55minutes.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fiftyfive.wicket.datetime;
+
+
+import fiftyfive.wicket.BaseWicketTest;
+
+import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class CalendarControlTest extends BaseWicketTest
+{
+    private static final Logger logger = LoggerFactory.getLogger(
+        CalendarControlTest.class
+    );
+
+    @Test
+    public void testCalendarControl() throws Exception
+    {
+        _tester.startPage(CalendarControlTestPage.class);
+        _tester.assertRenderedPage(CalendarControlTestPage.class);
+        
+        _tester.assertResultPage(
+            CalendarControlTestPage.class,
+            "CalendarControlTestPage-expected.html"
+        );
+    }
+}
