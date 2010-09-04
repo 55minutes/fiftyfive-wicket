be2a4b6263c85391462bd7f7acc21a56caa5d201
Matt Brictson <matt@55minutes.com>
2010-09-04 00:56:17 +0000

    Change references back to private.
    git-svn-id: http://opensource.55minutes.com/svn/java/trunk/fiftyfive-wicket@443 3a26569e-5468-0410-bb2f-b495330926e5

--- src/main/java/fiftyfive/wicket/resource/MergedResourceBuilder.java
+++ src/main/java/fiftyfive/wicket/resource/MergedResourceBuilder.java
@@ -94,7 +94,7 @@ public class MergedResourceBuilder
     private String _media;
     
     private String _path;
-    protected List<ResourceReference> _references;
+    private List<ResourceReference> _references;
     
     /**
      * Creates an empty builder object. See the
