96cd9e659e75eb2b65cb5ae62325137660b1f7c4
Matt Brictson <matt@55minutes.com>
2010-10-04 01:03:21 +0000

    Add comprehensive javadoc.
    git-svn-id: http://opensource.55minutes.com/svn/java/trunk/fiftyfive-wicket@520 3a26569e-5468-0410-bb2f-b495330926e5

--- src/main/java/fiftyfive/wicket/model/CountMessageModel.java
+++ src/main/java/fiftyfive/wicket/model/CountMessageModel.java
@@ -27,7 +27,49 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 
 /**
- * TODO
+ * Selects an appropriate localized string based on whether a count value
+ * is zero, one or greater than one. This makes it easier to produce singluar
+ * and plural phrases.
+ * <p>
+ * For example, consider the case where we would like to set one of these
+ * three feedback messages. Notice how the phrasing is slightly different
+ * depending on the number of items. Furthermore in the latter two examples
+ * we would like to embed a hyperlink to a bookmarkable page.
+ * <ul>
+ * <li>There are no friends in your network.</li>
+ * <li>There is <u>one friend</u> in your network.</li>
+ * <li>There are <u>5 friends</u> in your network.</li>
+ * </ul>
+ * <p>
+ * Normally in Wicket you would accomplish this using tedious if/else
+ * statements and string concatenation, conditionally visible
+ * fragments/containers, or the incredibly arcane syntax of Java's
+ * {@link java.text.ChoiceFormat ChoiceFormat} (and note that you'd have to
+ * carefully escape the "&lt;" and "&gt;" if you want to include HTML markup
+ * as we need for the hyperlinks in this example). All of these
+ * approaches are clumsy, and it would be hard for a non-programmer to localize
+ * or edit the strings.
+ * <p>
+ * With CountMessageModel there is a much easier way: simply declare three
+ * separate localizable strings in the properties file of your page or panel.
+ * <pre class="example">
+ * friends.zero = There are no friends in your network.
+ * friends.one = There is &lt;a href="${href}"&gt;one friend&lt;/a&gt; in your network.
+ * friends.many = There are &lt;a href="${href}"&gt;${count} friends&lt;/a&gt; in your network.</pre>
+ * <p>
+ * And your Java would be:
+ * <pre class="example">
+ * new CountMessageModel("friends", pageOrPanelObject, numFriends)
+ *     setLink(FriendsPage.class);</pre>
+ * <p>
+ * CountMessageModel selects the appropriate localized string depending on the
+ * value of {@code numFriends}, and subtitutes values for
+ * <code>${href}</code> and <code>${count}</code> automatically.
+ * <p>
+ * You can now use this model in a label (also consider
+ * {@link fiftyfive.wicket.basic.CountLabel CountLabel} as a shortcut for this
+ * common use case), or call {@link #getObject getObject()} to get the
+ * interpolated string value for use in a feedback message.
  * 
  * @since 2.0
  */
@@ -39,6 +81,20 @@ public class CountMessageModel extends AbstractReadOnlyModel<String>
     private Class<? extends Page> _linkPage;
     private PageParameters _linkParams;
     
+    /**
+     * Constructs a CountMessageModel that will choose the appropriate
+     * localized string from a properties file.
+     * 
+     * @param messageKey The key that will be consulted in the properties file.
+     *                   The key will have ".zero", ".one" or ".many" appended
+     *                   to it depending on the value of the {@code count}.
+     * @param component The Wicket component that will be used for finding
+     *                  the properties file. Usually this is the page or panel
+     *                  where you plan to use the model.
+     * @param count The number that will be used in the message. This will
+     *              be used to substitute any <code>${count}</code> expressions
+     *              in the message.
+     */
     public CountMessageModel(String messageKey,
                              Component component,
                              IModel<? extends Number> count)
@@ -63,6 +119,10 @@ public class CountMessageModel extends AbstractReadOnlyModel<String>
         );
     }
     
+    /**
+     * Detaches the count model passed into the constructor and other
+     * internal state.
+     */
     @Override
     public void detach()
     {
@@ -71,12 +131,19 @@ public class CountMessageModel extends AbstractReadOnlyModel<String>
         super.detach();
     }
 
+    /**
+     * Returns the message after all interpolation has been performed.
+     */
     public String getObject()
     {
         return _stringModel.getObject();
     }
     
     /**
+     * Sets the bookmarkable page that will be used to generate a link within
+     * the message. A special <code>${href}</code> variable will be exposed
+     * for use within the message.
+     * 
      * @return {@code this} to allow chaining
      */
     public CountMessageModel setLink(Class<? extends Page> page)
@@ -85,6 +152,10 @@ public class CountMessageModel extends AbstractReadOnlyModel<String>
     }
     
     /**
+     * Sets the bookmarkable page class and page parameters that will be used
+     * to generate a link within the message. A special <code>${href}</code> 
+     * variable will be exposed for use within the message.
+     * 
      * @return {@code this} to allow chaining
      */
     public CountMessageModel setLink(Class<? extends Page> page,
@@ -95,12 +166,22 @@ public class CountMessageModel extends AbstractReadOnlyModel<String>
         return this;
     }
     
+    /**
+     * Returns the current value of the count model. This is exposed as
+     * <code>${count}</code> within the message.
+     */
     public int getCount()
     {
         Number num = _countModel.getObject();
         return num != null ? num.intValue() : 0;
     }
     
+    /**
+     * Returns the URL to the bookmarkable page specified in a previous call
+     * to {@link #setLink(Class,PageParameters) setLink()}. This is exposed as
+     * <code>${href}</code> within the message. If {@code setLink()} was not
+     * called, then this value will be {@code null}.
+     */
     public CharSequence getHref()
     {
         if(_linkPage != null)
@@ -112,6 +193,10 @@ public class CountMessageModel extends AbstractReadOnlyModel<String>
         return null;
     }
     
+    /**
+     * Returns either ".zero", ".one", or ".many" depending on the current
+     * value of the count model.
+     */
     public String getResourceSuffix()
     {
         int count = getCount();
