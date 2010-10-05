858146cb00559b3b12656d89da13cd1195e37b84
Matt Brictson <matt@55minutes.com>
2010-10-05 05:01:39 +0000

    Add javadoc for `CountLabel`. I think all Java classes are now completely documented.
    git-svn-id: http://opensource.55minutes.com/svn/java/trunk/fiftyfive-wicket@523 3a26569e-5468-0410-bb2f-b495330926e5

--- src/main/java/fiftyfive/wicket/basic/CountLabel.java
+++ src/main/java/fiftyfive/wicket/basic/CountLabel.java
@@ -23,7 +23,37 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 
 /**
- * TODO
+ * Displays a label with up to three different variations based on
+ * whether a numeric model is zero, one, or greater than one. This is
+ * incredibly useful for printing count messages where the phrasing needs
+ * to change according to singular and plural rules, like this:
+ * <ul>
+ * <li>There are no friends in your network.</li>
+ * <li>There is <u>one friend</u> in your network.</li>
+ * <li>There are <u>5 friends</u> in your network.</li>
+ * </ul>
+ * <p>
+ * In addition, the label can contain a hyperlink that you specify by
+ * calling {@link #setLink(Class,PageParameters) setLink()}.
+ * <p>
+ * To achieve the output shown in the example above, you would first add
+ * these entries to your localizable properties file associated with your
+ * page or panel:
+ * <pre class="example">
+ * friends.zero = There are no friends in your network.
+ * friends.one = There is &lt;a href="${href}"&gt;one friend&lt;/a&gt; in your network.
+ * friends.many = There are &lt;a href="${href}"&gt;${count} friends&lt;/a&gt; in your network.</pre>
+ * <p>
+ * Then, construct the label and add it to your page or panel like so:
+ * <pre class="example">
+ * add(new CountLabel("friends", numFriendsModel)
+ *     .setLink(MyFriendsPage.class));</pre>
+ * <p>
+ * It's that simple.
+ * <p>
+ * This class is a simple Label wrapper around a CountMessageModel, which
+ * does most of the work. Refer to the class documentation of
+ * {@link CountMessageModel} for more details.
  * 
  * @since 2.0
  */
@@ -31,6 +61,20 @@ public class CountLabel extends Label
 {
     private CountMessageModel _messageModel;
     
+    /**
+     * Constructs a label that will a display zero, singular or plural
+     * variation based on the value of the specified {@code count} model.
+     * 
+     * @param id The wicket:id of the label component. This is will also
+     *           be used as the message key to look up the message strings
+     *           in the localized properties file.
+     * 
+     * @param count A number that will be used to choose either the
+     *              <code>id.zero</code>, <code>id.one</code> or
+     *              <code>id.many</code> string from the properties file. It
+     *              will also be made available to the string as an
+     *              interpolated <code>${count}</code> variable.
+     */
     public CountLabel(String id, IModel<? extends Number> count)
     {
         super(id);
@@ -39,6 +83,7 @@ public class CountLabel extends Label
     }
     
     /**
+     * @see CountMessageModel#setLink(Class)
      * @return {@code this} to allow chaining
      */
     public CountLabel setLink(Class<? extends Page> page)
@@ -47,6 +92,7 @@ public class CountLabel extends Label
     }
     
     /**
+     * @see CountMessageModel#setLink(Class,PageParameters)
      * @return {@code this} to allow chaining
      */
     public CountLabel setLink(Class<? extends Page> page, PageParameters params)
