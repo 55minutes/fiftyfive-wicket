0a8607f03b71204b37a0552c8629a2e9dc9980bd
Matt Brictson <matt@55minutes.com>
2010-11-06 05:15:11 +0000

    Add a `TransientModel` class that might come in handy when writing unit tests.
    git-svn-id: http://opensource.55minutes.com/svn/java/trunk/fiftyfive-wicket@617 3a26569e-5468-0410-bb2f-b495330926e5

---
+++ test/src/main/java/fiftyfive/wicket/test/TransientModel.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2010 55 Minutes (http://www.55minutes.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fiftyfive.wicket.test;
+
+import org.apache.wicket.model.IModel;
+
+/**
+ * Holds a value that will not be serialized. This is helpful
+ * in unit tests where you need to construct a quick and dirty model for a
+ * non-serializable object, and therefore {@code Model.of()} will not work.
+ * This class is not intended for production code.
+ */
+public class TransientModel<T> implements IModel<T>
+{
+    private transient T _object;
+    
+    /**
+     * Convience method for constructing a TransientModel instance.
+     * These statements are equivalent:
+     * <pre class="example">
+     * IModel&lt;String&gt; = TransientModel.of(value);
+     * IModel&lt;String&gt; = new TransientModel&lt;String&gt;(value);</pre>
+     */
+    public static <T> TransientModel<T> of(T value)
+    {
+        return new TransientModel<T>(value);
+    }
+    
+    /**
+     * Constructs a model that will hold the given value.
+     */
+    public TransientModel(T value)
+    {
+        super();
+        setObject(value);
+    }
+    
+    public T getObject()
+    {
+        return _object;
+    }
+    
+    public void setObject(T value)
+    {
+        _object = value;
+    }
+    
+    public void detach()
+    {
+        // pass
+    }
+}
