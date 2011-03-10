f49505e04ea5769c46b92dfce1de61c808145845
Matt Brictson <matt@55minutes.com>
2011-03-10 20:49:09 +0000

    Explicitly use maven 2.x for now since the build fails under 3.x. This is a temporary workaround. Addresses #77.
    git-svn-id: http://opensource.55minutes.com/svn/java/trunk/fiftyfive-wicket@736 3a26569e-5468-0410-bb2f-b495330926e5

--- archetype/ci-test.sh
+++ archetype/ci-test.sh
@@ -5,7 +5,7 @@
 
 mkdir temp
 cd temp
-mvn archetype:generate -B -U \
+mvn2 archetype:generate -B -U \
     -DarchetypeGroupId=com.55minutes \
     -DarchetypeArtifactId=fiftyfive-wicket-archetype \
     -DarchetypeRepository=http://opensource.55minutes.com/maven \
@@ -16,4 +16,4 @@ mvn archetype:generate -B -U \
     -Dpackage=fiftyfive.test \
     -Dproject_name=Test && \
 cd test-project && \
-mvn -B test
+mvn2 -B test
