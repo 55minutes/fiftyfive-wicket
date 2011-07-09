4241995dc78f8b801cdda58d9ed52b42620756dd
Richa Avasthi <ravasthi@55minutes.com>
2011-07-09 16:17:13 +0000

    Upgrade fiftyfive-wicket trunk to jQuery 1.6.2 and jQuery UI 1.8.14. Addresses #115.
    git-svn-id: http://opensource.55minutes.com/svn/java/trunk/fiftyfive-wicket@830 3a26569e-5468-0410-bb2f-b495330926e5

--- fiftyfive-wicket-js/src/main/java/fiftyfive/wicket/js/JavaScriptDependencySettings.java
+++ fiftyfive-wicket-js/src/main/java/fiftyfive/wicket/js/JavaScriptDependencySettings.java
@@ -122,15 +122,15 @@ public class JavaScriptDependencySettings
         
         this.jQueryResource = new PackageResourceReference(
             JavaScriptDependencySettings.class,
-            "lib/jquery-1.6.1/jquery.noconflict.min.js"
+            "lib/jquery-1.6.2/jquery.noconflict.min.js"
         );
         this.jQueryUIResource = new PackageResourceReference(
             JavaScriptDependencySettings.class,
-            "lib/jquery-ui-1.8.13/jquery-ui.min.js"
+            "lib/jquery-ui-1.8.14/jquery-ui.min.js"
         );
         this.jQueryUICSSResource = new PackageResourceReference(
             JavaScriptDependencySettings.class,
-            "lib/jquery-ui-1.8.13/themes/redmond/jquery-ui-1.8.13.redmond.css"
+            "lib/jquery-ui-1.8.14/themes/redmond/jquery-ui-1.8.14.redmond.css"
         );
     }
     
