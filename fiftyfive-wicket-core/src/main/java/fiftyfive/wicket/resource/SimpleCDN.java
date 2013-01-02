92fefcac112be12f0c1c676617cd6faf30b03759
Matt Brictson <matt@55minutes.com>
2013-01-02 11:45:39 -0800

    White space fixes

--- fiftyfive-wicket-core/src/main/java/fiftyfive/wicket/resource/SimpleCDN.java
+++ fiftyfive-wicket-core/src/main/java/fiftyfive/wicket/resource/SimpleCDN.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package fiftyfive.wicket.resource;
 
 import org.apache.wicket.protocol.http.WebApplication;
